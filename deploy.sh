5c137425e50b74ac57938dad7568d890d89e9dc9
mudbungie <mudbungie@mudbungie.net>
2017-03-10 16:19:06 -0800

    deploy script, installs some programs, updates configs

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Test to see what package managers are available.
+declare -a installers=("apt" "dnf")
+for installer in "${installers[@]}"; do
+	if which $installer; then
+		pkgmgr=$installer
+		echo "Package manager $installer detected."
+	fi
+done
+
+# Programs that I want installed.
+programs="vim wget curl python3"
+echo "Installing the following programs: $programs."
+echo sudo $pkgmgr install -y $programs
+if sudo $pkgmgr install -y $programs; then
+	echo "Installation successful."
+else:
+	echo "Installation failed."
+fi
+
+# Back up current confs and download mine.
+declare -a confs=(".bashrc" ".vimrc" ".sqliterc")
+for conf in "${confs[@]}"; do
+	if [ -e "$HOME/$conf" ]; then
+		echo "Backing up $HOME/$conf to $HOME/$conf.bak"
+		mv $HOME/$conf $HOME/$conf.bak
+	fi
+	echo "Installing $HOME/$conf"
+	wget -qO "$HOME/$conf" "https://www.mudbungie.net/$conf"
+done
+	
