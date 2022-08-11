daabdf0b5f367f19932a36378eb5c70f50cd3427
mudbungie <mudbungie@gmail.com>
2022-08-10 19:04:09 -0700

    Change list declaration

--- deploy.sh
+++ deploy.sh
@@ -143,7 +143,7 @@ function ensure_path_is_correct {
 }
 
 function install_app_config_files {
-    declare -a appconfigs=$(ls -A app_config)
+    appconfigs=$(ls -A app_config)
     for appconfig in "${appconfigs[@]}"; do
         backup_file $appconfig
         cp "~/userconf/app_config/$appconfig" "~/"
